a4b4716ea9cd75b22fc99a395e5c40dd79c7df65
HarshitSinghai <harshitksinghai.official@gmail.com>
2024-09-28 15:41:39 +0530

    Microservices Architecture

---
+++ department-service/src/main/java/com/harshitksinghai/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,31 @@
+package com.harshitksinghai.departmentservice.repository;
+
+import com.harshitksinghai.departmentservice.model.Department;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class DepartmentRepository {
+    private List<Department> departments
+            = new ArrayList<>();
+
+    public Department addDepartment(Department department) {
+        departments.add(department);
+        return department;
+    }
+
+    public Department findById(Long id) {
+        return departments.stream()
+                .filter(department ->
+                        department.getId().equals(id))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    public List<Department> findAll() {
+        return departments;
+    }
+
+}
